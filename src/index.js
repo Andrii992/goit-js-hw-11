db960c67d953741b30ec1e1c1dea02d6316efb01
Andrii992 <andrey.kursom.na.tishiny@gmail.com>
2022-09-27 22:37:10 +0300

    +

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import '../css/main.css';
+import APIService from './fetchImages';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.search-form');
+const loadMore = document.querySelector('.load-more');
+const galleryImages = document.querySelector('.gallery');
+const apiService = new APIService();
+
+form.addEventListener('submit', onSearch);
+loadMore.addEventListener('click', onLoadMore);
+
+loadMore.setAttribute(`disabled`, true);
+loadMore.classList.add('hidden');
+
+function onSearch(event) {
+  event.preventDefault();
+  apiService.searchQuery = event.target.elements.searchQuery.value;
+  const inputV = apiService.searchQuery.trim();
+  if (inputV === '') {
+    return;
+  }
+  loadMore.setAttribute(`disabled`, true);
+  // loadMore.classList.add('hidden')
+
+  apiService.resetPage();
+  apiService.fetchHits().then(data => {
+    const imagesHits = data.totalHits;
+    Notiflix.Notify.success(`Hooray! We found ${imagesHits} images`);
+    galleryImages.innerHTML = '';
+    const hits = data.hits;
+    render(hits);
+    lightbox.refresh();
+    // console.log(imagesHits)
+    apiService.incrementCountHits(imagesHits)
+    loadMore.removeAttribute(`disabled`, true);
+    loadMore.classList.remove('hidden');
+  });
+}
+
+function onLoadMore() {
+  apiService.fetchHits().then(data => {
+    render(data.hits);
+    lightbox.refresh();
+    const renderedMarkup = document.querySelectorAll('.photo-card');
+    // console.log(renderedMarkup.length)
+    // console.log(apiService.totalRenderHits)
+    if (renderedMarkup.length >= apiService.totalRenderHits) {
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      loadMore.setAttribute(`disabled`, true);
+    }
+    // console.log(totalHits)
+  });
+}
+
+function render(data) {
+  const markup = data
+    .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+      return `
+  <a class='photo-card' href="${largeImageURL}">
+  <div class="img-wrapper">
+    <img src='${webformatURL}' alt='${tags}' loading='lazy' />
+  </div>
+  <div class='info'>
+    <p class='info-item'>
+      <b>Likes</b>
+      <span>${likes}</span>
+    </p>
+    <p class='info-item'>
+      <b>Views</b>
+      <span>${views}</span>
+    </p>
+    <p class='info-item'>
+      <b>Comments</b>
+      <span>${comments}</span>
+    </p>
+    <p class='info-item'>
+      <b>Downloads</b>
+      <span>${downloads}</span>
+    </p>
+  </div>
+</a>`;
+    })
+    .join('');
+  galleryImages.insertAdjacentHTML('beforeend', markup);
+}
+
+const lightbox = new SimpleLightbox('.gallery a');
